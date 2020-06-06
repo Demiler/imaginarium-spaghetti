fa5637f4bd65058962f1d91c5566a00b57ff960f
demiler <seerpout@gmail.com>
2020-06-06 15:24:09 +0300

    start page added. Scollbar style moved to seprate file

---
+++ scrollbar.css
@@ -0,0 +1,10 @@
+::-webkit-scrollbar {
+  width: 6px;
+}
+::-webkit-scrollbar-track {
+  background-color: #131323;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #333343;
+}
