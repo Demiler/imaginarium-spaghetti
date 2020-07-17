2b40a83c5600323eebae4eb872cd341be6d97eb0
demiler <seerpout@gmail.com>
2020-07-17 21:45:21 +0300

    Smashed every class to ImApp. Now lobby works. Next step - game it self

--- src/lobby.js
+++ src/lobby.js
@@ -24,51 +24,71 @@ players.push(new Player("player1", "0.png", "ready", 0));
 players.push(new Player("player2", "0.png", "notReady", 0));
 players.push(new Player("player3", "0.png", "notReady", 0));
 
-class Players extends LitElement {
-  static get properties() {
-    return {
-      players: { type: Array } //host player is always first
-    };
-  }
-
-  constructor() {
-    super();
-    this.players = players;
-  }
-
-  render() {
-    return html`
-    ${this.players.map(player => html`
-      <div class="player ${player.status}">
-        <img class="playerImage" src="../images/avatars/${player.icon}">
-        <div class="playerName">${player.name}</div>
-      </div>
-    `)}
-    `;
-  }
-
-  createRenderRoot() { return this }
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 }
 
-class Lobby extends LitElement {
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+class ImApp extends LitElement {
   static get properties() {
     return {
-      name: { type: String },
-      over: { type: Object },
-      host: { type: Object },
-      btn: { type: Boolean }
+      state: { type: String },
+      players: { type: Array },
+      host: { type: Player },
+      lobbyBtn: { type: Boolean },
+      checkLock: { type: Boolean },
+      gameLeader: { type: Player },
+      gameLeaderId: { type: Number },
+      gameQueue: { type: Array },
     };
   }
 
   constructor() {
     super();
-    this.name = "lobby";
-    this.over = { pList: new Players(), btnState: false };
+    this.state = "lobby";
+    this.players = players;
     this.host = players[0];
-    this.btn = false;
+    this.lobbyBtn = false;
+    this.lobbyLock = false;
   }
 
   render() {
+    switch (this.state) {
+      case 'lobby': this.lobbyChecker(); return this.lobby(); break;
+      default: return html`not found`; 
+    }
+  }
+
+  async gameChecker() {
+  }
+
+  initGame() {
+    this.gameQueue = shuffle(this.players);
+    this.gameLeaderId = 0;
+    this.gameLeader = this.gameQueue[0];
+    this.state = this.gameLeader === this.host ? 'game guess' : 'game wait';
+  }
+
+  async lobbyChecker() {
+    if (this.checkLock === true) return;
+    this.lobbyLock = true;
+    while (
+      this.players.filter(player => player.status === 'notReady').length !== 0
+    ) await sleep(1000);
+    initGame();
+    this.checkLock = false;
+  }
+
+  lobby() {
     return html`
       <header>
         <span class="nameTag">Imaginarium</span>
@@ -79,54 +99,37 @@ class Lobby extends LitElement {
       <div class="container">
         <h1>List of players</h1>
 
-        <im-players class="playerList"></im-players>
+        <div class="playerList">
+          ${this.players.map(player => html`
+            <div class="player ${player.status}">
+              <img class="playerImage" src="../images/avatars/${player.icon}">
+              <div class="playerName">${player.name}</div>
+            </div>
+          `)}
+        </div>
+
         <div class="playerCounter">
           <span class="ready">
-            ${this.over.pList.players.filter(player => player.status === "ready").length}
+            ${this.players.filter(player => player.status === "ready").length}
           </span>/<span class="total">
-            ${this.over.pList.players.length}
+            ${this.players.length}
           </span>
         </div>
 
         <button class="readyButton ${this.host.status}"
-        @click="${this.readyButton}">
+        @click="${this.lobbyReadyButton}">
           ${this.host.status === "ready" ? "not ready" : "ready"}
         </button>
       </div>
-    `;
+    `
   }
 
-  readyButton() {
-    this.host.status = (this.host.status === "ready" ? "notReady" : "ready");
-    this.btn = !this.btn;
-  }
-
-  createRenderRoot() { return this }
-}
-
-class ImApp extends LitElement {
-  static get properties() {
-    return {
-      state: { type: String },
-      player: { type: Object }
-    };
-  }
-
-  constructor() {
-    super();
-    this.player = new Player();
-    this.player.name = "You";
-    this.state = "lobby"
-  }
-
-  render() {
-    return html`<im-lobby></im-lobby>`;
+  lobbyReadyButton() {
+    this.host.status = this.host.status === "ready" ? "notReady" : "ready";
+    this.lobbyBtn = !this.lobbyBtn;
   }
 
   createRenderRoot() { return this }
 }
 
 customElements.define('im-app', ImApp); 
-customElements.define('im-lobby', Lobby); 
-customElements.define('im-players', Players); 
-//customElements.define('im-player', ); //
